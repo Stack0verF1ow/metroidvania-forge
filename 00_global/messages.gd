299c6b259ead4ee1e03b488cc60b92fb8389c73d
Stack0verF1ow <120239382+Stack0verF1ow@users.noreply.github.com>
2026-04-16 00:27:15 +0800

    实现玩家血量

--- 00_global/messages.gd
+++ 00_global/messages.gd
@@ -3,3 +3,4 @@ extends Node
 signal player_interacted( player : Player )
 signal player_healed( amount : int )
 signal input_hints_changed( hint : String )
+signal player_health_changed( hp: float, max_hp: float )
--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -23,8 +23,16 @@ var gravity : float = 980
 var gravity_weight : float = 1.0
 
 
-var hp : float = 20
-var max_hp : float = 20
+var hp : float = 20 :
+	set( value ):
+		hp = clampf( value, 0, max_hp )
+		Messages.player_health_changed.emit( hp, max_hp )
+		
+var max_hp : float = 20 :
+	set( value ):
+		max_hp = value
+		Messages.player_health_changed.emit( hp, max_hp )
+		
 var dash : bool = false
 var double_jump : bool = false
 var ground_slam : bool = false
@@ -38,9 +46,22 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Action"):
-		print("player actiond")
 		Messages.player_interacted.emit( self )
 	change_state( current_state.handle_input(event))
+	
+	# Get rid of later
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_MINUS:
+			if Input.is_key_pressed( KEY_SHIFT ):
+				max_hp -= 10
+			else:
+				hp -=2
+		elif event.keycode == KEY_EQUAL :
+			if Input.is_key_pressed( KEY_SHIFT ):
+				max_hp += 10
+			else:
+				hp += 2
+	#end get rid of
 
 func _process(_delta: float) -> void:
 	update_direction()
